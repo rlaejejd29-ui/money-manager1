54aa36776b90c7bf04e8109e0b190dd7a5711c21
rlaejejd29-ui <rlaejejd29@gmail.com>
2026-03-22 21:22:06 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -328,6 +328,7 @@ export default function App() {
               <option>소프트웨어비</option>
               <option>보험</option>
               <option>행사입점비</option>
+              <option>행정처리비</option>
             </select>
 
             <select value={payment} onChange={(e) => setPayment(e.target.value)} style={input}>
