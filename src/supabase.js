26002d4eb0c26952cad065cae9cb7ac4beedf2ee
rlaejejd29-ui <rlaejejd29@gmail.com>
2026-03-24 20:18:34 +0900

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://sjnwuaxsvylyvsvemzuf.supabase.co";
-const supabaseKey = "sb_publishable_2-H2HkD1Co2zt48xJ8LNIg_ZE2NaLJ1";
+const supabaseAnonKey = "sb_publishable_2-H2HkD1Co2zt48xJ8LNIg_ZE2NaLJ1";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
